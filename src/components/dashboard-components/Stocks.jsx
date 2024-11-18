183b681bda6a590e02bd8cd33f229ac16c9c6c16
MohamedMetwalli5 <mohamedmetwalli5@gmail.com>
2024-11-18 20:12:38 +0200

    Adding the top tips component

--- src/components/dashboard-components/Stocks.jsx
+++ src/components/dashboard-components/Stocks.jsx
@@ -5,16 +5,16 @@ import StockTips from '../stocks-components/StockTips';
 
 const Stocks = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4 mt-5 bg-emerald-100 rounded-lg">
+    <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 gap-4 p-4 mt-5 bg-emerald-100 rounded-lg">
       <div className="flex justify-center items-center shadow-lg border rounded-lg p-4">
         <StocksChart />
       </div>
       
-      <div className="flex justify-center items-center shadow-lg border rounded-lg p-4">
+      <div className="flex justify-center items-center shadow-lg border rounded-lg p-1">
         <StocksWatchList />
       </div>
       
-      <div className="flex justify-center items-center shadow-lg border rounded-lg p-4">
+      <div className="flex justify-center shadow-lg border rounded-lg">
         <StockTips />
       </div>
     </div>
--- src/components/stocks-components/StockTips.jsx
+++ src/components/stocks-components/StockTips.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React from 'react';
+import { FaRegSquareCaretDown, FaRegSquareCaretUp } from "react-icons/fa6";
 
 const StockTips = () => {
-  return (
-    <div>StockTips</div>
-  )
-}
+  const TopTips = ["tytyuytyutyutuytu", "trgfdg"];
 
-export default StockTips
+  return (
+    <div className="flex flex-col bg-slate-100 w-full p-4 rounded-lg h-full">
+      <div className="flex-grow">
+        
+        <div className="mb-4">
+          <div className="flex gap-2 items-center p-1">
+            <img
+              src="/src/assets/images/PersonalPhoto.png"
+              alt="Profile Photo"
+              className="w-8 h-8 rounded-full"
+            />
+            <h3 className="font-bold text-black mr-2">Metwalli</h3>
+          </div>
+          <p className="bg-white shadow-md rounded-md p-3 m-1">{TopTips[0]}</p>
+          <div className='flex pl-3'>
+            <FaRegSquareCaretUp className='text-green-500 cursor-pointer'/>
+            <FaRegSquareCaretDown className='text-red-500 cursor-pointer'/>
+          </div>
+        </div>
+
+        <div className="mb-4">
+          <div className="flex gap-2 items-center p-1">
+            <img
+              src="/src/assets/images/PersonalPhoto.png"
+              alt="Profile Photo"
+              className="w-8 h-8 rounded-full"
+            />
+            <h3 className="font-bold text-black mr-2">Hassan</h3>
+          </div>
+          <p className="bg-white shadow-md rounded-md p-3 m-1">{TopTips[1]}</p>
+          <div className='flex pl-3'>
+            <FaRegSquareCaretUp className='text-green-500 cursor-pointer'/>
+            <FaRegSquareCaretDown className='text-red-500 cursor-pointer'/>
+          </div>
+        </div>
+      </div>
+
+      {/* The Input Section */}
+      <div className="mt-auto">
+        <input
+          placeholder="Share Tips"
+          className="text-lg p-2 w-full border rounded-md"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default StockTips;
