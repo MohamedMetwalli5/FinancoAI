40bf239ac467d17e4a910e1cd22affd3bb079e57
MohamedMetwalli5 <mohamedmetwalli5@gmail.com>
2024-11-19 01:38:33 +0200

    Updating the notifications bell

--- src/components/notifications/Notifications.jsx
+++ src/components/notifications/Notifications.jsx
@@ -3,8 +3,8 @@ import { FaBell } from "react-icons/fa";
 const Notifications = () => {
   return (
     <>
-        <div className="relative flex items-center justify-center">
-                <FaBell className="text-xl cursor-pointer" />
+        <div className="relative flex items-center justify-center cursor-pointer">
+                <FaBell className="text-xl" />
                 <span className="absolute ml-4 bg-red-500 rounded-full w-2 h-2"></span>
         </div>
     </>
