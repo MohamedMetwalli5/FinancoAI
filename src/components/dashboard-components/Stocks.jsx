12c1330291f609732a567410eca21464896e06cd
MohamedMetwalli5 <mohamedmetwalli5@gmail.com>
2024-11-18 19:07:49 +0200

    Adding the stocks performance and the watchlist components

--- src/components/dashboard-components/Stocks.jsx
+++ src/components/dashboard-components/Stocks.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
-import StocksChart from '../stocks-components/StocksChart'
-import StocksWatchList from '../stocks-components/StocksWatchList'
+import React from 'react';
+import StocksChart from '../stocks-components/StocksChart';
+import StocksWatchList from '../stocks-components/StocksWatchList';
+import StockTips from '../stocks-components/StockTips';
 
 const Stocks = () => {
   return (
-    <div className='flex space-x-2 mt-5 bg-emerald-100'>
-        <StocksChart/>
-        <StocksWatchList/>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4 mt-5 bg-emerald-100 rounded-lg">
+      <div className="flex justify-center items-center shadow-lg border rounded-lg p-4">
+        <StocksChart />
+      </div>
+      
+      <div className="flex justify-center items-center shadow-lg border rounded-lg p-4">
+        <StocksWatchList />
+      </div>
+      
+      <div className="flex justify-center items-center shadow-lg border rounded-lg p-4">
+        <StockTips />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Stocks
+export default Stocks;
--- src/components/stocks-components/StocksWatchList.jsx
+++ src/components/stocks-components/StocksWatchList.jsx
@@ -1,30 +1,67 @@
 import React from 'react'
+import { FaArrowUp } from "react-icons/fa6";
 
 const StocksWatchList = () => {
   return (
-    <div className='flex-1 bg-slate-100 rounded-lg p-2 relative'>
-        <div className='flix flex-col'>
-            <h1 className='text-lg font-bold'>Watchlist</h1>
-
+    <div className='flex-1 bg-white rounded-lg p-2'>
+        <div className='flex-col'>
+            <div className='flex justify-between items-center mb-5'>
+                <h1 className='text-lg font-bold'>Watchlist</h1>
+                <button className='bg-blue-100 rounded-lg p-2 text-gray-500'>Add New</button>
+            </div>
             <div className='flex m-auto justify-between items-center border-y p-1'>
                 <div>
                     <h2 className='text-md font-bold'>ADS</h2>
-                    <h4 className='text-sm'>ADS</h4>
+                    <h4 className='text-sm'>Adidas AG</h4>
                 </div>
                 <div>
                     <h2 className='text-md font-bold'>53,49$</h2>
-                    <h4 className='text-sm text-green-500'>0.76%</h4>
+                    <div className='flex text-green-500'>
+                        <FaArrowUp className='mr-1'/>
+                        <h4 className='text-sm'>0.76%</h4>
+                    </div>
                 </div>
             </div>
             
             <div className='flex m-auto justify-between items-center border-y p-1'>
                 <div>
                     <h2 className='text-md font-bold'>AAPL</h2>
-                    <h4 className='text-sm'>AAPL</h4>
+                    <h4 className='text-sm'>Apple Inc</h4>
                 </div>
                 <div>
                     <h2 className='text-md font-bold'>193,36$</h2>
-                    <h4 className='text-sm text-green-500'>0.76%</h4>
+                    <div className='flex text-green-500'>
+                        <FaArrowUp className='mr-1'/>
+                        <h4 className='text-sm'>0.76%</h4>
+                    </div>
+                </div>
+            </div>
+
+            <div className='flex m-auto justify-between items-center border-y p-1'>
+                <div>
+                    <h2 className='text-md font-bold'>NYSE</h2>
+                    <h4 className='text-sm'>Nike Inc</h4>
+                </div>
+                <div>
+                    <h2 className='text-md font-bold'>108,49$</h2>
+                    <div className='flex text-red-500'>
+                        <FaArrowUp className=' mr-1'/>
+                        <h4 className='text-sm'>11.83%</h4>
+                    </div>
+                </div>
+            </div>
+
+            <div className='flex m-auto justify-between items-center border-y p-1'>
+                <div>
+                    <h2 className='text-md font-bold'>BMW</h2>
+                    <h4 className='text-sm'>Bayerische MW</h4>
+                </div>
+                <div>
+                    <h2 className='text-md font-bold'>110,19$</h2>
+                    <div className='flex text-red-500'>
+                        <FaArrowUp className=' mr-1'/>
+                        <h4 className='text-sm'>1.09%</h4>
+                    </div>
                 </div>
             </div>
 
