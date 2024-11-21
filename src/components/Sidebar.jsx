95518c9a63664c9348b4f20ca762d53503da1111
MohamedMetwalli5 <mohamedmetwalli5@gmail.com>
2024-11-21 23:22:09 +0200

    Adding the website logo and icon

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,8 +8,9 @@ const SideBar = ({email}) => {
   return (
     <div className="fixed left-0 top-0 h-screen w-64 bg-white text-gray-800 p-4 text-center font-bold rounded-r-lg shadow-md flex flex-col justify-between">
       <div>
-        <h1 onClick={() => navigate('/dashboard', {state: {email: email}})} className="text-2xl font-semibold mb-8 text-gray-800 cursor-pointer">FinancoAI</h1>
-        
+        <h1 onClick={() => navigate('/dashboard', {state: {email: email}})} className="text-2xl font-semibold  text-gray-800 cursor-pointer">FinancoAI</h1>
+        <img  onClick={() => navigate('/dashboard', {state: {email: email}})} src="\src\assets\TheWebsiteLogo.svg" alt="The Logo" className="items-center justify-center cursor-pointer mb-10 w-15 h -8" />
+
         <button onClick={() => navigate('/dashboard', {state: {email: email}})} className="flex items-center justify-start w-full p-3 mt-4 mb-2 rounded-lg bg-gray-100 hover:bg-gray-200 transition-all">
           <FaClipboardList className="mr-4 text-xl text-gray-700"/> Dashboard
         </button>
