d87e3ec1fc11ba43ddca29bcd0e1ad0fd2056849
MohamedMetwalli5 <mohamedmetwalli5@gmail.com>
2024-11-24 14:18:50 +0200

    Adding the transactions schema and some of the route handlers

---
+++ backend/src/models/transaction.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+// The transaction schema
+const transactionSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  userId: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  recurring: {
+    type: Boolean,
+    required: true,
+  }
+}, {
+  timestamps: true
+});
+
+
+const Transaction = mongoose.model('Transaction', transactionSchema);
+
+export default Transaction;
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import db from "./config/db.js";
 import User from "./models/user.js";
+import Transaction from "./models/transaction.js";
 
 dotenv.config();
 
@@ -10,18 +11,39 @@ const port = process.env.PORT || 5000;
 db();
 
 const app = express();
-
+app.use(express.json());
 
 app.listen(5000, () => {
     console.log(`Server is running on port ${port}`)
 });
 
 app.get("/", async (req, res) => {
-    const users = await User.find();
-    res.send(users);
+    try {
+        const users = await User.find();
+        res.status(200).send(users);
+    } catch (error) {
+        res.status(400).send({ error: error.message });
+    }
 });
 
+
 app.post("/signup", async (req, res) => {
-    const newUser = await User.create(req.body);
-    res.send(newUser);
+    try {
+        const newUser = await User.create(req.body);
+        res.status(201).send(newUser);
+    } catch (error) {
+        res.status(400).send({ error: error.message });
+    }
 });
+
+
+app.post("/transactions", async (req, res) => {
+    try {
+        const newTransaction = await Transaction.create(req.body);
+        res.status(201).send(newTransaction);
+    } catch (error) {
+        res.status(400).send({ error: error.message });
+    }
+});
+
+
