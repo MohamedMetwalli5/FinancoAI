baf41bebfdcfee49ea54394177ef6caf630cc883
MohamedMetwalli5 <mohamedmetwalli5@gmail.com>
2024-12-05 02:22:12 +0200

    Adding some updates

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -20,7 +20,7 @@ const Header = () => {
     <>
       <div className="flex items-center justify-between pb-4 p-1">
         <div>
-          <h1 className="text-2xl font-bold text-black-500 font-serif">Good morning, {sharedUserEmail} </h1>
+          <h1 className="text-2xl font-bold text-black-500 font-serif">Good morning, {name} </h1>
           <h2 className="font-semibold text-gray-500">Long time no see</h2>
         </div>
       
@@ -30,7 +30,7 @@ const Header = () => {
           </div>
           {/* {ThereAreNotifications == true? <Notifications /> : <NoNotifications />} */}
           <div onClick={() => navigate('/settings')} className="flex items-center justify-center p-4 cursor-pointer">
-            <h3 className="font-bold text-black mr-2">{sharedUserEmail} </h3>
+            <h3 className="font-bold text-black mr-2">{name} </h3>
             <img
               src="/src/assets/images/PersonalPhoto.png"
               alt="Profile Photo"
