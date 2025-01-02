325cfec1f2830285c401fd97f1f3c739db62dfb6
MohamedMetwalli5 <mohamedmetwalli5@gmail.com>
2025-01-02 02:18:14 +0200

    Migrate from local storage to cloud storage

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -6,12 +6,16 @@ dotenv.config();
 const MONGODB_URL = process.env.MONGODB_URL;
 
 const db = async () => {
-    try{
-        const connection = await mongoose.connect(MONGODB_URL);
-        console.log(`mongodb connected: ${connection.connection.host}`);
-    }catch (error){
-        console.error(error);
+    try {
+        const connection = await mongoose.connect(MONGODB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log(`MongoDB connected: ${connection.connection.host}`);
+    } catch (error) {
+        console.error("MongoDB connection error:", error);
+        process.exit(1);
     }
-}
+};
 
 export default db;
