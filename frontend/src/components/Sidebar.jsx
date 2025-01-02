32081178994209112cbd1612f54d4bc9f44de947
MohamedMetwalli5 <mohamedmetwalli5@gmail.com>
2025-01-02 22:15:06 +0200

    Correct SVG import to resolve module export error

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import { FaClipboardList, FaRobot, FaRegNewspaper, FaGear, FaRightFromBracket } from 'react-icons/fa6';
 import { useNavigate } from 'react-router-dom';
-
+import TheWebsiteLogo from "../assets/TheWebsiteLogo.svg";
 
 
 const SideBar = () => {
@@ -11,7 +11,7 @@ const SideBar = () => {
     <div className="fixed left-0 top-0 h-screen w-64 bg-white text-gray-800 p-4 text-center font-bold rounded-r-lg shadow-md flex flex-col justify-between">
       <div>
         <h1 onClick={() => navigate('/dashboard')} className="text-2xl font-semibold  text-gray-800 cursor-pointer">FinancoAI</h1>
-        <img  onClick={() => navigate('/dashboard')} src="\src\assets\TheWebsiteLogo.svg" alt="The Logo" className="items-center justify-center cursor-pointer mb-10 w-15 h -8" />
+        <img  onClick={() => navigate('/dashboard')} src={TheWebsiteLogo} alt="The Logo" className="items-center justify-center cursor-pointer mb-10 w-15 h -8" />
 
         <button onClick={() => navigate('/dashboard')} className="flex items-center justify-start w-full p-3 mt-4 mb-2 rounded-lg bg-gray-100 hover:bg-gray-200 transition-all">
           <FaClipboardList className="mr-4 text-xl text-gray-700"/> Dashboard
