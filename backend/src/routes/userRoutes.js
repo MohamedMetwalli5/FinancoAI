3cb3d91d4e2ba40dfc5ced046519bb4e20d26991
MohamedMetwalli5 <mohamedmetwalli5@gmail.com>
2025-01-13 21:25:36 +0200

    Fix a bug in the user deletion process to prevent error

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -2,6 +2,9 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import User from "../models/user.js";
+import Tips from "../models/tips.js";
+import SubscribedStocks from "../models/subscribed-stocks.js";
+import Transaction from "../models/transaction.js";
 import authenticateToken from "../middlewares/authMiddleware.js";
 import generateToken from "../utils/tokenUtils.js";
 import bcrypt from 'bcryptjs';
@@ -74,12 +77,30 @@ router.delete("/:email", authenticateToken, async (req, res) => {
   try {
     const { email } = req.params;
 
-    const Theuser = await User.findOne({ email });
-    if (!Theuser) {
+    const theUser = await User.findOne({ email });
+    if (!theUser) {
       return res.status(404).send({ error: "User not found" });
     }
-
+    // To delete the user
     await User.deleteOne({ email });
+
+    // To delete the user tips
+    const theUserTips = await Tips.findOne({ email });
+    if (theUserTips) {
+      await Tips.deleteMany({ email });
+    }
+
+    // To delete the user subscribed stocks
+    const theUserSubscribedStocks = await SubscribedStocks.findOne({ email });
+    if (theUserSubscribedStocks) {
+      await SubscribedStocks.deleteMany({ email });
+    }
+    
+    // To delete the user transactions
+    const theUserTransactions = await Transaction.findOne({ email });
+    if (theUserTransactions) {
+      await Transaction.deleteMany({ email });
+    }
 
     res.status(200).send("User is Deleted Successfully");
   } catch (error) {
