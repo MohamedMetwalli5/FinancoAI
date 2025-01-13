8ee4a7b3db8186009f468bfc2f2c83db5df3a18a
MohamedMetwalli5 <mohamedmetwalli5@gmail.com>
2025-01-13 13:02:36 +0200

    Fix UI bug with the logo display

--- frontend/src/components/stocks-components/StocksChart.jsx
+++ frontend/src/components/stocks-components/StocksChart.jsx
@@ -52,36 +52,36 @@ const StocksChart = () => {
     fetchSubscribedStocks();
   }, [])
   
-  // I used mock data here for demonstration purposes; the same approach can be applied to live data in the stocks watchlist
+  // I used mock data here for demonstration purposes; the same approach can be applied to live data in the stocks watchlist.
   const mockStockData = [
     {
       symbol: "AAPL",
       data: [
-        { datetime: "2024-11-20 10:00:00", open: 150.75, high: 152.30, low: 150.10, close: 121.80, volume: 450000 },
-        { datetime: "2024-11-20 10:01:00", open: 151.80, high: 152.00, low: 150.90, close: 141.30, volume: 420000 },
-        { datetime: "2024-11-20 10:02:00", open: 151.30, high: 151.90, low: 150.50, close: 251.60, volume: 380000 },
-        { datetime: "2024-11-20 10:03:00", open: 151.60, high: 152.50, low: 151.10, close: 102.20, volume: 500000 },
-        { datetime: "2024-11-20 10:04:00", open: 152.20, high: 152.80, low: 151.70, close: 170.40, volume: 510000 }
+        { datetime: "2024-11-20 10:00:00", open: 316.00, high: 320.00, low: 315.00, close: 318.00, volume: 450000 },
+        { datetime: "2024-11-20 10:01:00", open: 318.00, high: 321.00, low: 317.00, close: 319.50, volume: 420000 },
+        { datetime: "2024-11-20 10:02:00", open: 319.00, high: 322.00, low: 318.00, close: 318.50, volume: 380000 },
+        { datetime: "2024-11-20 10:03:00", open: 318.50, high: 320.50, low: 317.50, close: 318.75, volume: 500000 },
+        { datetime: "2024-11-20 10:04:00", open: 318.80, high: 319.80, low: 317.80, close: 318.00, volume: 510000 }
       ]
     },
     {
       symbol: "AMZN",
       data: [
-        { datetime: "2024-11-20 10:00:00", open: 2800.00, high: 2805.00, low: 2790.00, close: 102.50, volume: 120000 },
-        { datetime: "2024-11-20 10:01:00", open: 2802.50, high: 2810.00, low: 2800.00, close: 157.00, volume: 125000 },
-        { datetime: "2024-11-20 10:02:00", open: 2807.00, high: 2812.00, low: 2805.00, close: 300.50, volume: 115000 },
-        { datetime: "2024-11-20 10:03:00", open: 2809.50, high: 2815.00, low: 2808.00, close: 205.00, volume: 130000 },
-        { datetime: "2024-11-20 10:04:00", open: 2812.00, high: 2816.00, low: 2810.00, close: 240.50, volume: 140000 }
+        { datetime: "2024-11-20 10:00:00", open: 216.00, high: 219.00, low: 215.00, close: 218.00, volume: 120000 },
+        { datetime: "2024-11-20 10:01:00", open: 218.00, high: 220.00, low: 217.00, close: 219.00, volume: 125000 },
+        { datetime: "2024-11-20 10:02:00", open: 219.00, high: 221.00, low: 218.00, close: 218.50, volume: 115000 },
+        { datetime: "2024-11-20 10:03:00", open: 218.50, high: 220.50, low: 218.00, close: 218.25, volume: 130000 },
+        { datetime: "2024-11-20 10:04:00", open: 218.30, high: 219.30, low: 217.30, close: 218.10, volume: 140000 }
       ]
     },
     {
       symbol: "GOOGL",
       data: [
-        { datetime: "2024-11-20 10:00:00", open: 2800.00, high: 2805.00, low: 2790.00, close: 202.50, volume: 120000 },
-        { datetime: "2024-11-20 10:01:00", open: 2802.50, high: 2810.00, low: 2800.00, close: 107.00, volume: 125000 },
-        { datetime: "2024-11-20 10:02:00", open: 2807.00, high: 2812.00, low: 2805.00, close: 150.50, volume: 115000 },
-        { datetime: "2024-11-20 10:03:00", open: 2809.50, high: 2815.00, low: 2808.00, close: 125.00, volume: 130000 },
-        { datetime: "2024-11-20 10:04:00", open: 2812.00, high: 2816.00, low: 2810.00, close: 140.50, volume: 140000 }
+        { datetime: "2024-11-20 10:00:00", open: 190.00, high: 195.00, low: 189.00, close: 193.00, volume: 120000 },
+        { datetime: "2024-11-20 10:01:00", open: 193.00, high: 195.50, low: 192.00, close: 194.00, volume: 125000 },
+        { datetime: "2024-11-20 10:02:00", open: 194.00, high: 196.00, low: 192.50, close: 194.50, volume: 115000 },
+        { datetime: "2024-11-20 10:03:00", open: 194.50, high: 196.50, low: 193.00, close: 193.50, volume: 130000 },
+        { datetime: "2024-11-20 10:04:00", open: 193.50, high: 195.00, low: 193.00, close: 193.00, volume: 140000 }
       ]
     }
   ];
